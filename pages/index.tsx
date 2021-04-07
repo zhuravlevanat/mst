7f910857844aa31c0126e4be93541987136bc3c7
Natalia <nataliazh@wizardsdev.com>
2021-04-07 18:36:57 +0300

    added changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,22 +9,22 @@ export default function Ssr() {
 // The date returned here will be different for every request that hits the page,
 // that is because the page becomes a serverless function instead of being statically
 // exported when you use `getServerSideProps` or `getInitialProps`
-// export function getServerSideProps() {
-//     const store = initializeStore()
-//
-//     store.update()
-//
-//     return { props: { initialState: getSnapshot(store) } }
-// }
-Ssr.getInitialProps = async () => {
-    try {
-        const store = initializeStore()
+export function getServerSideProps() {
+    const store = initializeStore()
 
     store.update()
 
     return { props: { initialState: getSnapshot(store) } }
-    } catch (e) {
-        console.log('in page', e);
-        return {};
-    }
-};
+}
+// Ssr.getInitialProps = async () => {
+//     try {
+//         const store = initializeStore()
+//
+//     store.update()
+//
+//     return { props: { initialState: getSnapshot(store) } }
+//     } catch (e) {
+//         console.log('in page', e);
+//         return {};
+//     }
+// };
